f6a2b4bd7e27d285cc73474fef45afd9b4ffd596
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 21:42:24 +0300

    021_Автоматическое преобразование типов

--- script1.js
+++ script1.js
@@ -1,4 +1,57 @@
 "use strict";
 
-const PI = 3.14;
-console.log(10 * pi * 12);
+// let a = '5' + '2';
+// alert(a)
+//55
+
+// let a = '5' + 2;
+// alert(a);
+// 52
+
+// let a = 5 + '2';
+// alert(a);
+// 52
+
+// let a = 5 + 2;
+// alert(a);
+// 7
+
+// let a = '5' * '2';
+// alert(a);
+// 10
+
+// let a = '5' - '2';
+// alert(a);
+// 3
+
+// let a = '5' / '2';
+// alert(a);
+// 2.5
+
+// let a = '5' % '2';
+// alert(a);
+// 1
+
+// let a = '5s' * '2';
+// alert(a);
+// NaN
+
+// let a = '5s' + '2';
+// alert(a);
+// 5s2
+
+// let a = (-'5') + (-'2');
+// alert(a);
+// -7
+
+// let a = '5' * 1 + '2' * 1;
+// alert(a);
+// 7
+
+// let a = '5' * '1' + '2' * '1';
+// alert(a);
+// 7
+
+// let a = '' + 3 + 1;
+// alert(a);
+// 31
