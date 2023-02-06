c123d146e5b04ec357f2ac3b49b507d37a521141
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 22:43:38 +0300

    025_Преобразование логического типа

--- script1.js
+++ script1.js
@@ -1,18 +1,15 @@
 "use strict";
 
-let num1 = 10;
-let num2 = 20;
+alert(true + 3);
 
-alert(String(num1) + String(num2));
+alert(true + true);
 
+alert(true - true);
 
-let num = 23423423;
-let str = String(num);
-alert(str.length); 
+alert(true + false);
 
-let number = 23423423;
-let str2 = String(num);
+alert('1' + true);
 
-let number2 = 23423423;
-let str3 = String(num);
-alert(str2.length + str3.length); 
+alert( String(true) + 1 );
+
+alert( String(true) + Number(true) );
