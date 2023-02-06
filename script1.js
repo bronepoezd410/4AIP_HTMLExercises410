2b7802201793e2d624b6c7902b2b78de91afff68
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 21:08:32 +0300

    015_Сложение строк

--- script1.js
+++ script1.js
@@ -1,7 +1,12 @@
 "use strict";
 
 
-let str = "Mako";
-let str2= "Theo";
+let str = "!!!";
+let str2= "java";
+let str3= "script";
 alert(str);
-alert(str2);
+alert(str2 + str3);
+
+let str4= "hello";
+let str5= "world";
+alert(str4 + '' + str5);
