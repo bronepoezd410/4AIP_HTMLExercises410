89a57ebc1a0a71309c1bc1d8b9e07d0d84475e33
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 21:32:45 +0300

    020_Константы

--- script1.js
+++ script1.js
@@ -1,9 +1,4 @@
 "use strict";
-let num1 = 123;
-let num2 = 456;
-let num3 = 789;
-console.log(num1, num2, num3);
 
-console.log(123);
-console.log('123');
-console.log(false);
+const PI = 3.14;
+console.log(10 * pi * 12);
