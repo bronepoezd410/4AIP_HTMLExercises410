2b11f57967483339fd2dad824a5d3853e9ef80b9
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 20:39:56 +0300

    008_Математические операции

--- script1.js
+++ script1.js
@@ -1,3 +1,20 @@
 "use strict";
-let a = 1+2+3;
-alert(a);
+// let a = 10;
+// let b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+// //
+// let a = 10, b = 5;
+// result = a + b;
+// alert(result);
+// //
+// let a = 1, b = 2, c = 3;
+// alert(a + b + c);
+// //
+let a = 10, b = 5;
+c = a - b;
+let d = 7;
+result = c + d;
+alert(result);
