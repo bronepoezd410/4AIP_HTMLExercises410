8df8b1d7ecaa7fa57360ab30391e3fb834669bb6
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 20:50:07 +0300

    009_Приоритет математических операций

--- script1.js
+++ script1.js
@@ -1,20 +1,37 @@
 "use strict";
-// let a = 10;
-// let b = 2;
-// alert(a + b);
-// alert(a - b);
-// alert(a * b);
-// alert(a / b);
-// //
-// let a = 10, b = 5;
-// result = a + b;
-// alert(result);
-// //
-// let a = 1, b = 2, c = 3;
-// alert(a + b + c);
-// //
-let a = 10, b = 5;
-c = a - b;
-let d = 7;
-result = c + d;
-alert(result);
+let a = 5 + 5 * 3;
+alert(a);
+//20
+let b = 5 + 5 * 3 + 3;
+alert(b);
+//2
+let c = 8 / 2 + 2;
+alert(c);
+//6
+let d = 8 + 2 / 2;
+alert(d);
+//9
+let e = 8 / 2 * 2;
+alert(e);
+//8
+let f = 8 * 4 / 2 / 2;
+alert(f);
+//8
+let g = (2 + 3) * (2 + 3);
+alert(g);
+//25
+let r = (2 + 3) * 2 + 3;
+alert(r);
+//13
+let q= 2 * (2 + 4 * (3 + 1));
+alert(q);
+//36
+let u = 2 * 8 / 4;
+alert(u);
+//4
+let i= (2 * 8) / 4;
+alert();
+//4
+let o = 2 * (8 / 4);
+alert(o);
+//4
