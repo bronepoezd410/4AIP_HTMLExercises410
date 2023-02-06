daf301a5bf2fe1cec0f13a8da30bb9f62d21fafb
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-06 21:09:54 +0300

    016_Длина строки

--- script1.js
+++ script1.js
@@ -1,12 +1,5 @@
 "use strict";
 
 
-let str = "!!!";
-let str2= "java";
-let str3= "script";
-alert(str);
-alert(str2 + str3);
-
-let str4= "hello";
-let str5= "world";
-alert(str4 + '' + str5);
+let str = 'abcdeasdfasdfa';
+alert(str.length); 
