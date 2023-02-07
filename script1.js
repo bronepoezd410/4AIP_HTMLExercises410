723ebdd507f6c9702d59f929b1316899f57aab0c
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-07 20:13:51 +0300

    032_Неточные вычисления

--- script1.js
+++ script1.js
@@ -1,65 +1,12 @@
 "use strict";
 
-// let num = 10;
-// num++;
-// num++;
-// num--;
-// alert(num);
+// alert(0.1 * 0.2);
+// 0.020000...4
+// alert(0.3 - 0.1);
+// 1.99999...8
 
-// let num = 3;
-// alert(++num);
-// 4
-
-// let num = 3;
-// alert(num++);
-// 3
-
-// let num = 3;
-// alert(--num);
-// 2
-
-// let num = 3;
-// alert(num--);
-// 3
-
-// let num1 = 3;
-// let num2 = ++num1;
-// alert(num1);
-// alert(num2);
-// 4
-// 4
-
-// let num1 = 3;
-// let num2 = num1++;
-// alert(num1);
-// alert(num2);
-// 3
-// 4
-
-
-// let num1 = 3;
-// let num2 = --num1;
-// alert(num1);
-// alert(num2);
-// 2
-// 2
-
-// let num1 = 3;
-// let num2 = num1--;
-// alert(num1);
-// alert(num2);
-// 3
-// 2
-
-// let num1 = 3;
-// num1++;
-// let num2 = num1--;
-// alert(num1++);
-// alert(--num2);
-// 3
-// 3
-
-
+// alert((0.1 * 0.2).toFixed(2));
+// 0.02
 
 
 
