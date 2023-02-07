66737dff440c1aacfa1d58ec8ac778580a324f92
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-07 21:30:58 +0300

    035_Поиск ошибок в коде

--- script1.js
+++ script1.js
@@ -1,7 +1,56 @@
 "use strict";
 
-document.write('<b>КАКОЙ-НИБУДЬ ТЕКСТ</b> <br>');
-document.write('<i>КАКОЙ-НИБУДЬ ТЕКСТ КУРСИВОМ</i> <br>');
-let str = '123123asdfadfa123';
-document.write('<i>' + str + '</i> <br>');
-document.write('1 <br> 2 <br> 3 <br> 4 <br> 5 <br> ');
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + Number(num1 + num2));
+
+
+// let a = 1;
+// let b = 2;
+// console.log(a + b /* + c*/);
+
+// let num = '123';
+// let sum = (+num[0] + +num[1] + +num[2]);
+// console.log(sum);
+
+// let num11 = 123;
+// num11 = String(num11);
+// console.log(num11[0]);
+
+// let x = 0;
+// console.log(++x);
+
+// let num12 = 123;
+// console.log(String(num.length));
+
+// let secondsinday = 24 * 60  * 60;
+// console.log(secondsinday);
+
+// let num123 = 123;
+// let str = String(num);
+// console.log(num.length);
+
+// let num321 = 123;
+// let str321 = String(num321);
+// console.log(str321[str321.length]);
+
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+
+let num = 123;
+let str = String(num);
+console.log(str[str.length-1]);
+
+let a = '123';
+let b = '456';
+let s = Number(a) + Number(b);
+
+console.log(s);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+
+console.log(aaa + bbb + ссс);
