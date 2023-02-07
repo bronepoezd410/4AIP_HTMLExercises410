3371f5727e2345d4078150dced3d20211667a103
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-07 19:59:08 +0300

    029_Строки с цифрами

--- script1.js
+++ script1.js
@@ -1,9 +1,16 @@
 "use strict";
 
-let a = 'abcde';
-alert(a[0]);
-alert(a[2]);
-alert(a[4]);
 
-let b = 3;
-alert(a[b]); // выведет 'd'
+let x = '12345';
+alert(Number(x[0])+Number(x[1])+Number(x[2])+Number(x[3])+Number(x[4]));
+
+let r = String(12345);
+alert(Number(r[0])+Number(r[1])+Number(r[2])+Number(r[3])+Number(r[4]));
+
+let f = String(12345);
+alert(Number(f[0])*Number(f[1])*Number(f[2])*Number(f[3])*Number(f[4]));
+
+let g = String(12345);
+alert((g[4])+(g[3])+(g[2])+(g[1])+(g[0]));
+
+
