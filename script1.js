8d4ca9b8eba4d1b93fbb10f754104fd7ff2fcc6c
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-08 18:31:51 +0300

    36 в процессе...

--- script1.js
+++ script1.js
@@ -1,56 +1,7 @@
 "use strict";
 
-// let num1 = 1;
-// let num2 = 2;
-// console.log('сумма: ' + Number(num1 + num2));
-
-
-// let a = 1;
-// let b = 2;
-// console.log(a + b /* + c*/);
-
-// let num = '123';
-// let sum = (+num[0] + +num[1] + +num[2]);
-// console.log(sum);
-
-// let num11 = 123;
-// num11 = String(num11);
-// console.log(num11[0]);
-
-// let x = 0;
-// console.log(++x);
-
-// let num12 = 123;
-// console.log(String(num.length));
-
-// let secondsinday = 24 * 60  * 60;
-// console.log(secondsinday);
-
-// let num123 = 123;
-// let str = String(num);
-// console.log(num.length);
-
-// let num321 = 123;
-// let str321 = String(num321);
-// console.log(str321[str321.length]);
-
-// let num = 123;
-// let str = String(num);
-// console.log(str.length);
-
-
-let num = 123;
-let str = String(num);
-console.log(str[str.length-1]);
-
-let a = '123';
-let b = '456';
-let s = Number(a) + Number(b);
-
-console.log(s);
-
-let aaa = 1;
-let bbb = 2;
-let ccc = 3;
-
-console.log(aaa + bbb + ссс);
+alert('sfd' + 24*60*60);
+alert(30*24*60*60);
+alert(30*12*24*60*60);
+alert(24*60);
+alert(30*12*24*60);
