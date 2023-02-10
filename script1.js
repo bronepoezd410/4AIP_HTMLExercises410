438fa14a1a7c26bac116e9c4e6e52ba3234c48e3
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-10 13:53:19 +0300

    046_Оператор delete в массивах

--- script1.js
+++ script1.js
@@ -1,12 +1,7 @@
 "use strict";
 
-let arr = ['a', 'b', 'c'];
-
-let key = 2;
-
-console.log(arr[key]);
-
-let arr1 = [1, 2, 3, 4, 5];
-let key1 = 1;
-let key2 = 2;
-alert(arr1[key1] + arr1[key2])
+let arr = ['a', 'b', 'c', 'd', 'e'];
+delete arr[1];
+delete arr[2];
+alert(arr.length);
+console.log(arr);
