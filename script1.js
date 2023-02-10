4d2342ae40806dc86508f17de07ba25dedad0f3f
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-10 13:43:43 +0300

    042_Добавление элементов массива

--- script1.js
+++ script1.js
@@ -1,20 +1,14 @@
 "use strict";
 
-let arr = ['a', 'b', 'c'];
+let arr = [];
 
-arr[0] = '1';
-arr[1] = '-2';
-arr[2] = '-3';
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+
 console.log(arr);
 
 let arr1 = [1, 2, 3];
-arr1[0] += 1;
-arr1[1] += 1;
-arr1[2] += 1;
+arr1[arr1.length] = 4;
+arr1[arr1.length] = 5;
 console.log(arr1);
-
-let arr2 = [1, 2, 3];
-arr[0]++;
-arr[1]++;
-arr[2]++;
-console.log(arr2);
