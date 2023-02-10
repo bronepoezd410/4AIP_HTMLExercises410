924ffc07a964c403e55484c3b5eb9e779fa2de31
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-10 13:16:34 +0300

    038_Mассивы

--- script1.js
+++ script1.js
@@ -1,35 +1,9 @@
 "use strict";
 
-//№ 1
-let r = 10;
-let s = (r*3,14)**2
-alert(s)
+let Myarr = [1, 2, 3];
+alert(Myarr);
+console.log(Myarr);
 
-//№ 2
-let a = 10;
-let s1 = a**a
-alert(s1)
-
-//№ 3
-let a1 = 4;
-let b1 = 3;
-let s3 = a1*b1;
-alert(s)
-
-//№ 4
-let a2 = 4;
-let b2 = 3;
-let p = 2*(a2+b2)
-alert()
-
-//№ 5
-let tc = 20;
-let fc = (tc * 9/5) + 32
-alert(fc)
-
-//№ 6
-let fc1 = 451;
-let tc1 = (fc-32)/1.8;
-alert(tc1)
-
-
+let Yourarr = ['a', 'b', 'c'];
+alert(Yourarr);
+console.log(MyaYourarrrr);
