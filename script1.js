de14cb5f1859f8bf50b5794011ff2b4cbbfee9b7
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-10 13:45:36 +0300

    043_Разреженные массивы

--- script1.js
+++ script1.js
@@ -2,13 +2,6 @@
 
 let arr = [];
 
-arr[0] = 1;
-arr[1] = 2;
-arr[2] = 3;
-
-console.log(arr);
-
-let arr1 = [1, 2, 3];
-arr1[arr1.length] = 4;
-arr1[arr1.length] = 5;
-console.log(arr1);
+arr[3] = 'a';
+arr[8] = 'b';
+alert(arr.length);
