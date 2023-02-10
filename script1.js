4708e0b748c861d42791d07a2f7489846a7956bf
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-10 14:06:20 +0300

    047_Поиск ошибок

--- script1.js
+++ script1.js
@@ -1,7 +1,11 @@
 "use strict";
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-delete arr[1];
-delete arr[2];
-alert(arr.length);
-console.log(arr);
+let arr = [1, 2, 3, 4, 5];
+// последний элемент это индекс - 1
+console.log(arr[arr.length-1]);
+// элементы начинаются с нуля
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]); 
+//массив задается в квадратных скобках
+console.log(arr.length);
+//грамматическая ошибка в слове length
+console.log(arr.length);
