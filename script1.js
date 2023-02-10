cb2467679418d0372401a7ac5f9d896167911e87
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-10 13:27:37 +0300

    040_длина массива

--- script1.js
+++ script1.js
@@ -1,11 +1,5 @@
 "use strict";
 
-let arr = [1,2,3];
-alert(arr[0]);
-alert(arr[1]);
-alert(arr[2]);
-alert(arr[0]+arr[1]+arr[2]);
-
-
-let arr1 = [a,b,c,d];
-alert(arr1[0] + '+' +arr1[1]+'+'+arr1[2]+ '+' + arr1[3]);
+let arr = ['a', 'b', 'c', 2, 5, 3, true];
+alert(arr.length)
+alert(arr[arr.length - 1])
