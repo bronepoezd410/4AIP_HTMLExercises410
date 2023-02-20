bef47433a84948fb610d03daf5f22706bdfe1054
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-20 21:01:06 +0300

    082_Логическое ИЛИ

--- script1.js
+++ script1.js
@@ -1,18 +1,18 @@
 "use strict";
 
-// let num = 3;
-// if (num > 0 && num < 5) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');}
-// let num1 = 11;
-// if (num1 >= 10 && num1 <=20) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');}
-// let num3 = -1;
-// let num2 = 4;
-// if (num2 >= 3 && num3 <=1) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');}
+1
+---
+2
++++
+3
++++
+4
++++
+5
++++
+6
++++
+7
+---
+8
++++
