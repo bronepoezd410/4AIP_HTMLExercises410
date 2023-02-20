c5b8d03cf51a140741448799ba4539bce87f6bba
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-20 20:21:30 +0300

    077_Равенство строк

--- script1.js
+++ script1.js
@@ -1,17 +1,10 @@
 "use strict";
 
+let test = 'abc';
+let test2 = 'abc';
 
-// let test1 = 1;
-// let test2 = 2;
-// if (test2 > test1) {
-// 	console.log('test2 > test1');
-// } else {
-// 	console.log('test2 < test1');
-// }
-// let test3 = 1;
-// let test4 = 2;
-// if (test3 == test4) {
-// 	console.log('test3 == test4');
-// } else {
-// 	console.log('test3 != test4');
-// }
+if (test == test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
