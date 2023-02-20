1b23eee61e331f3fa072fc59c9bf55048208e1af
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-20 20:26:25 +0300

    079_Равенство по значению и типу

--- script1.js
+++ script1.js
@@ -1,10 +1,52 @@
 "use strict";
 
-let test1 = '123';
-let test2 = 123;
-
+let test1 = '3';
+let test2 = '3';
+		
 if (test1 == test2) {
-	console.log('+++'); // сработает это
+	console.log('+++');
 } else {
 	console.log('---');
 }
+тру
+
+let test1 = '3';
+let test2 = '3';
+		
+if (test1 === test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+тру
+
+let test1 = 3;
+let test2 = '3';
+		
+if (test1 == test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+тру
+
+
+let test1 = 3;
+let test2 = '3';
+		
+if (test1 === test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+фолс
+
+let test1 = 3;
+let test2 = 3;
+		
+if (test1 === test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+тру
