cc7e77e8162c704f38ffa9ad908cfcb29b530d71
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-27 20:06:34 +0300

    088_Сокращенная форма проверки на истину

--- script1.js
+++ script1.js
@@ -1,57 +1,9 @@
 "use strict";
-let test = 1;
-		
-if (test == true) {
+
+let test = true;
+
+if (test) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
-+++
-let test = 0;
-		
-if (test == true) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
----
-let test = 1;
-		
-if (test == false) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
----
-let test = 1;
-		
-if (test != true) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
----
-let test = '';
-		
-if (test == false) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
----
-let test;
-		
-if (test == true) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
-+++
-let test = 3 * 'a';
-		
-if (test == true) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
----
