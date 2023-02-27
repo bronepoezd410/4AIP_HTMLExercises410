9d7c93384b8c0fecd77a9e9eb9abe7b04c3a82fa
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-27 20:12:47 +0300

    090_Общая сокращенная форма проверки

--- script1.js
+++ script1.js
@@ -1,24 +1,87 @@
 "use strict";
-let test = true;
+// let test = 3;
 
-if (!test) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// +++
+// let test = 'abc';
 
-let test = true;
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// +++
+// let test = '';
 
-if (!test) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// ---
+// let test = 3 * 'abc';
 
-let test = true;
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// ---
 
-if (test) {
-	console.log('+++');
-} else {
-	console.log('---');
-}
+// let test = null;
+
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// ---
+
+// let test = false;
+
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// ---
+
+// let test;
+
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// ---
+
+// let test = 0;
+
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// ---
+
+// let test = '0';
+
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// +++
+
+// let test = -1;
+
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// +++
