c74ae9d7b2baa97da037156960a5e83d0e85206c
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-27 19:58:42 +0300

    085_Инвертирование логических выражений

--- script1.js
+++ script1.js
@@ -1,26 +1,7 @@
-// "use strict";
+"use strict";
 
-// let num = 3;
-		
-// if (num > 5 && num < 10 || num == 20) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// слева направо
-
-// if (num > 5 || num > 0 && num < 3) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// справа налево
-
-// if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {
-// 	console.log('+++');
-// } else {
-// 	console.log('---');
-// }
-// num > 10 && num < 20 
-// num > 20 && num < 30
-// или или в
+if (!(num1 >= 0 || num2 <= 10)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
