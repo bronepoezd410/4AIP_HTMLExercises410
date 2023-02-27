22dfa96b54ffe574c56aa2a2acb91c98264dd706
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-27 19:54:19 +0300

    084_Группировка условий

--- script1.js
+++ script1.js
@@ -1,4 +1,26 @@
-"use strict";
+// "use strict";
 
-слева направо
-num > 1 && num < 5 || num == 0
+// let num = 3;
+		
+// if (num > 5 && num < 10 || num == 20) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// слева направо
+
+// if (num > 5 || num > 0 && num < 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// справа налево
+
+// if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+// num > 10 && num < 20 
+// num > 20 && num < 30
+// или или
