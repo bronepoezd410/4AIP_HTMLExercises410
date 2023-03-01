7bf1695e2bb5669de532cca211afbc6e7a744691
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 13:23:53 +0300

    102_Нюансы области видимости переменных в конструкциях if-else

--- script1.js
+++ script1.js
@@ -1,11 +1,57 @@
 "use strict";
 
-let age = 17;
-let adult
 
-if (age >= 18) {
-   adult = true;
-} else {
-   adult = false;
-}
-console.log(adult);
+//    let age = 17;
+//    let adult;
+
+//    if (age >= 18) {
+//       adult = true;
+//    } else {
+//       adult = false;
+//    }
+
+//    console.log(adult);
+
+
+//    let age = 17;
+//    let adult;
+
+//    if (age >= 18) {
+//       adult = true;
+//    } else {
+//       adult = false;
+//    }
+
+//    console.log(adult);
+
+
+//    let age = 17;
+//    let res;
+
+//    if (age >= 18) {
+//       if (age <= 23) {
+//          res = 'от 18 до 23';
+//       } else {
+//          res = 'больше 23';
+//       }
+//    } else {
+//       res = 'меньше 18';
+//    }
+
+//    console.log(res);
+
+
+   let age = 19;
+   let res;
+
+   if (age >= 18) {
+      if (age <= 23) {
+         res = 'от 18 до 23';
+      } else {
+         res = 'больше 23';
+      }
+   } else {
+      res = 'меньше 18';
+   }
+
+   console.log(res);
