9d822a33375ff979bd12a2717dad5ec1a8ddc5f9
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 12:16:49 +0300

    095_Вложенные конструкции if-else

--- script1.js
+++ script1.js
@@ -1,70 +1,15 @@
 "use strict";
 
+   let num = 35;
 
-let num = 12;
+   if (num >= 10 && num <= 99) {
 
-if (num <=31 && num >=1) 
-	if (num == 1) {
-		console.log('1');
-	} else if (num == 2) {
-		console.log('1');
-	} else if (num == 3) {
-		console.log('1');
-	} else if (num == 4) {
-		console.log('1');
-	} else if (num == 5) {
-		console.log('1');
-	} else if (num == 6) {
-		console.log('1');
-	} else if (num == 7) {
-		console.log('1');
-	} else if (num == 8) {
-		console.log('1');
-	} else if (num == 9) {
-		console.log('1');
-	} else if (num == 10) {
-		console.log('1');
-	} else if (num == 11) {
-		console.log('2');
-	} else if (num == 12) {
-		console.log('2');
-	} else if (num == 13) {
-		console.log('2');
-	} else if (num == 14) {
-		console.log('2');
-	} else if (num == 15) {
-		console.log('2');
-	} else if (num == 16) {
-		console.log('2');
-	} else if (num == 17) {
-		console.log('2');
-	} else if (num == 18) {
-		console.log('2');
-	} else if (num == 19) {
-		console.log('2');
-	} else if (num == 20) {
-		console.log('2');
-	} else if (num == 21) {
-		console.log('3');
-	} else if (num == 22) {
-		console.log('3');
-	} else if (num == 23) {
-		console.log('3');
-	} else if (num == 24) {
-		console.log('3');
-	} else if (num == 25) {
-		console.log('3');
-	} else if (num == 26) {
-		console.log('3');
-	} else if (num == 27) {
-		console.log('3');
-	} else if (num == 28) {
-		console.log('3');
-	} else if (num == 29) {
-		console.log('3');
-	} else if (num == 30) {
-		console.log('3');
-	} else if (num == 31) {
-		console.log('3');
-	} 
-else console.log('неверное значение переменной num');
+      num = String(num).split('');
+      let summary = Number(num[0]) + Number(num[1])
+
+    if (summary >= 9) {
+         alert('двухзначно');}
+	else {
+         alert('однозначно')}
+	
+   }
