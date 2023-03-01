54bf5d70574912b8ea1316cbabad8e5e83b51572
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 12:37:14 +0300

    099_Логические операции

--- script1.js
+++ script1.js
@@ -1,5 +1,19 @@
 "use strict";
 
-let num = 1;
-let res = num >=0 ? '1' : '2';
-console.log(res);
+//    let a = 2 * (3 - 1);
+//    let b = 6 - 2;
+//    let res = a == b;
+
+//    console.log(res)
+
+//    let a = 5 * (7 - 4);
+//    let b = 1 + 2 + 7;
+//    let res = a > b;
+
+//    console.log(res)
+
+   let a = 2 ** 4;
+   let b = 4 ** 2;
+   let res = a != b;
+   
+   console.log(res)
