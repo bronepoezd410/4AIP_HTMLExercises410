cce1b0617b08b221978305500fc680a915c9af35
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 13:34:00 +0300

    104_Проверка длины строк и массивов

--- script1.js
+++ script1.js
@@ -1,16 +1,8 @@
 "use strict";
 
 
-let min = 21;
-
-if (min >= 0 && min <= 20) {
-   console.log('1');
-}
-
-if (min >= 21 && min <= 41) {
-   console.log('2');
-}
-
-if (min >= 42 && min <= 60) {
-   console.log('3');
+let arr = [1, 2, 3];
+if (arr.length == 3) {
+   let x = arr[0] + arr[1] + arr[2];
+   console.log(x);
 }
