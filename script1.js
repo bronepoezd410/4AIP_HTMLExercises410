a3070d7eb712c9184fe8d99757f86fa2b32f703c
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 13:43:54 +0300

    108_Поиск ошибок в коде с условиями

--- script1.js
+++ script1.js
@@ -1,18 +1,113 @@
 "use strict";
 
+//    let num = 1;
+//    let num = 2;
 
-//    let num = 8;
-
-//    if (num % 2 == 0) {
-//       console.log('da')
+//    if (num + num === 3) {
+//       console.log('+++');
 //    } else {
-//       console.log('net')
+//       console.log('---');
 //    }
 
-   let num = 30;
+//    let num = '1';
+//    let num = '2';
 
-   if (num % 3 == 0) {
-      console.log('da')
+//    if (Number(num) + Number(num) === 3) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+//    let num = '1';
+//    let num = '2';
+
+//    if (Number(num) + Number(num) === 3) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 123;
+//    let str = String(num)
+
+//    if (str[0] == '1') {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 123;
+
+//    if (String(num)[0] === '1') {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 123;
+
+//    if (String(num)[0] === '1') {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 123;
+//    let first = String(num);
+
+//    if (first[0] === '1') {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 12;
+
+//    if (String(num).length === 2) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 12;
+//    let str11 = String(num);
+
+//    if (str11.length === 2) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 12;
+
+//    if (String(num).length === 2) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 12;
+
+//    if (String(num).length === 2) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+//    let num = 12;
+
+//    if (String(num).length === 2) {
+//       console.log('+++');
+//    } else {
+//       console.log('---');
+//    }
+
+   let num = '432342';
+
+   let sum1 = Number(num[0]) + Number(num[1]) + Number(num[2]);
+   let sum2 = Number(num[3]) + Number(num[4]) + Number(num[5]);
+
+   if (sum1 == sum2) {
+      console.log('summaries equal');
    } else {
-      console.log('net')
+      console.log('summaries not equal');
    }
