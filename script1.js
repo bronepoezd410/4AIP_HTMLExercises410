cdbc8113523058b295c0f2c791c1b1bb5709b303
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 13:39:59 +0300

    107_Проверка остатка от деления

--- script1.js
+++ script1.js
@@ -1,17 +1,17 @@
 "use strict";
 
-//    let num = 1231;
-//    let str = String(num)
 
-//    if (str[str.length - 1] != 0) {
+//    let num = 8;
+
+//    if (num % 2 == 0) {
 //       console.log('da')
 //    } else {
 //       console.log('net')
 //    }
 
-   let num = 4;
+   let num = 30;
 
-   if (num % 2 == 0) {
+   if (num % 3 == 0) {
       console.log('da')
    } else {
       console.log('net')
