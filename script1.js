7aa527d885d033eb3308007d647adea4dc19b0d1
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-01 13:11:12 +0300

    100_Функция confirm

--- script1.js
+++ script1.js
@@ -1,19 +1,9 @@
 "use strict";
 
-//    let a = 2 * (3 - 1);
-//    let b = 6 - 2;
-//    let res = a == b;
+let ok = confirm('Вы достигли 18 лет?');
 
-//    console.log(res)
-
-//    let a = 5 * (7 - 4);
-//    let b = 1 + 2 + 7;
-//    let res = a > b;
-
-//    console.log(res)
-
-   let a = 2 ** 4;
-   let b = 4 ** 2;
-   let res = a != b;
-   
-   console.log(res)
+if (ok) {
+   alert('да');
+} else {
+   alert('нет');
+}
