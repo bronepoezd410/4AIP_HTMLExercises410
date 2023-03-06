6aad2aab36aad58fa3b181e312014c5f6d1052c3
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-06 20:45:22 +0300

    122_Инструкция continue

--- script1.js
+++ script1.js
@@ -1,48 +1,18 @@
 "use strict";
 
-// let arr = [1, 2, 3, 4, 0, 5, 6];
+   
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// for (let i = 0; i <= arr.length - 1; i++) {
-//    if (arr[i] == 0) {
-//       break;
-//    } else {
-//       console.log(arr[i]);
-//    };
-// };
+for (let elem of arr) {
+   let result;
 
-// let arr = [1, 2, 3, 4, -2, 5, 6];
-// let res = 0;
-
-// for (let i = 0; i <= arr.length - 1; i++) {
-//    if (arr[i] < 0) {
-//       break;
-//    } else {
-//       res += arr[i];
-//    };
-// };
-// console.log(res)
-
-
-// let arr = [1, 2, 6, 4, 3, 5, 6];
-// let res = 0;
-
-// for (let i = 0; i <= arr.length - 1; i++) {
-//    if (arr[i] == 3) {
-//       console.log(i)
-//       break;
-//    } else {
-//       continue;
-//    };
-// };
-
-let num = 1, i = 1;
-
-for (i; ; i++) {
-   if (num + i > 100) {
-      break;
+   if (elem % 2 == 0) {
+      result = elem * elem;
+   } else if (elem % 3 == 0) {
+      result = elem * elem * elem;
    } else {
-      num += i;
-   };
-};
-console.log(num);
-console.log(i);
+      continue;
+   }
+
+   console.log(result);
+}
