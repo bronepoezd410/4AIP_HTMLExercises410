f2090174aea7af4cb40a224f25b99ef4ea742777
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-06 20:53:37 +0300

    129_Изменение объектов через цикл

--- script1.js
+++ script1.js
@@ -1,52 +1,21 @@
 "use strict";
 
-
 // console.log('');
 
-// let arr = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-// let arr = [1, 2, 3, 4, 5, 6, 7];
-// let newObj = {};
-
-// for (let i = 0; i <= 6; i++) {
-//    let key = arr[i];
-//    let value = arr[i]
-
-//    newObj[key] = value
-// };
-
-// console.log(newObj);
-
-// console.log('');
-
-// let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-// let newObj = {};
+// let obj = {x: 1, y: 2, z: 3};
 
 // for (let key in obj) {
-
-//    if (obj[key] % 2 == 0) {
-
-//       let newKey = key;
-//       let value = obj[key];
-//       newObj[newKey] = value;
-
-//    } else {
-//       continue
-//    };
+//    obj[key] = obj[key] ** 2;
 // };
 
-// console.log(newObj);
+// console.log(obj)
 
 // console.log('');
 
-let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
-let newObj = {};
+let obj = {x: 1, y: 2, z: 3};
 
 for (let key in obj) {
-
-   let newKey = obj[key];
-   let newValue = key;
-
-   newObj[newKey] = newValue;
+   obj[key] += 1
 };
-console.log(obj);
-console.log(newObj);
+
+console.log(obj)
