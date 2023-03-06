217aa0b12610bd47db688364ffe41203f71c660e
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-06 20:38:47 +0300

    118_Накопление элементов массивов в циклах

--- script1.js
+++ script1.js
@@ -1,31 +1,38 @@
 "use strict";
 
-// let res = 0;
+   // let res = 0
+   // let arr = [2, 5, 9, 3, 1, 4];
 
-// for (let i = 2; i <= 100; i++)
-//    if (i % 2 == 0) {
-//       res = res + i
-//    } else {
-//       continue;
-//    }
+   // for (let i = 0; i <= arr.length - 1; i++) {
+   //    res = res + arr[i]
+   // };
+   // console.log(res)
 
-// console.log(res)
+   // let res = 0
+   // let arr = [2, 5, 9, 3, 1, 4];
 
-// let res = 0;
+   // for (let i = 0; i <= arr.length - 1; i++) {
+   //    if (arr[i] % 2 == 0) {
+   //       res = res + arr[i]
+   //    } else {
+   //       continue;
+   //    }
+   // };
+   // console.log(res)
 
-// for (let i = 1; i <= 99; i++)
-//    if (i % 2 != 0) {
-//       res = res + i
-//    } else {
-//       continue;
-//    }
+   // let res = 0
+   // let arr = [2, 5, 9, 3, 1, 4];
 
-// console.log(res)
+   // for (let i = 0; i <= arr.length - 1; i++) {
+   //    res = res + arr[i] ** 2
+   // };
+   // console.log(res3)
 
-let res2 = 1;
 
-for (let i = 1; i <= 20; i++) {
-   res = res * i
-}
+   let res = 1;
+   let arr = [2, 5, 9, 3, 1, 4];
 
-console.log(res)
+   for (let i = 0; i <= arr.length - 1; i++) {
+      res = res * arr[i] 
+   };
+   console.log(res4)
