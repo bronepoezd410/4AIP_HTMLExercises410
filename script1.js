c452cb79d87fab953200d244c1fe106a67e59ef9
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-06 20:59:07 +0300

    134_Советы по написанию кода циклов

--- script1.js
+++ script1.js
@@ -1,29 +1,11 @@
 "use strict";
 
-// let arr = [1, 2, 3, 4, 5, 6];
+let arr = [10, 20, 30, 40, 21, 32, 51];
 
-// for (let i = 1; i <= arr.length; i++) {
-//     console.log([arr[i]]);
-// };
-
-// let res = 0
-
-// for (let i = 1; i <= arr.length; i++) {
-//     res = arr[i] + arr[i + 1];
-//     console.log(res);
-// };
-
-// for (let i = 1; i <= arr.length; i++) {
-//     console.log(arr[i - 2], arr[i - 1]);
-// };
-
-// for (let i = 1; i <= arr.length; i++) {
-//     console.log(arr[i - 2] + arr[i - 1]);
-// };
-
-let res = 0;
-
-for (let i = 1; i <= arr.length; i++) {
-    res = arr[i - 1] + arr[i] + arr[i + 1];
-    console.log(res)
+for (let i of arr) {
+   if (String(i)[0] == '1' || String(i)[0] == '2') {
+      console.log(i);
+   } else {
+      continue;
+   };
 };
