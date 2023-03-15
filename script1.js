7dad87b42cb140bc8e290f990cc8a01187fb6629
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 19:14:15 +0300

    Многомерные структуры

--- script1.js
+++ script1.js
@@ -1,30 +1,9 @@
 "use strict";
 
-let obj = {
-   1: {
-      1: 11,
-      2: 12,
-      3: 13,
-   },
-   2: {
-      1: 21,
-      2: 22,
-      3: 23,
-   },
-   3: {
-      1: 24,
-      2: 25,
-      3: 26,
-   },
+let students = {
+   'group1': ['name11', 'name12', 'name13'],
+   'group2': ['name21', 'name22', 'name23'],
+   'group3': ['name31', 'name32', 'name33'],
 };
 
-let result = 0;
-
-for (let key in obj) {
-   let obj3 = obj[key];
-   for (let obj1 in obj3) {
-      result += obj3[obj1]
-   };
-};
-
-console.log(result)
+console.log(students['group3'][0])
