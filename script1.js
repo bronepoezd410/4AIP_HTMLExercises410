4391609f3abd8d23639043e479be00f42a4e7d09
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 18:35:26 +0300

    Перебор многомерных массивов

--- script1.js
+++ script1.js
@@ -19,3 +19,26 @@
 // }
 
 // console.log(sum); // Output: 21
+
+let letarr = [
+   [
+     [1, 2],
+     [3, 4],
+   ],
+   [
+     [5, 6],
+     [7, 8],
+   ],
+ ];
+ 
+ let sum = 0;
+ 
+ for (let i = 0; i < letarr.length; i++) {
+   for (let j = 0; j < letarr[i].length; j++) {
+     for (let k = 0; k < letarr[i][j].length; k++) {
+       sum += letarr[i][j][k];
+     }
+   }
+ }
+ 
+ console.log(sum); // Output: 36
