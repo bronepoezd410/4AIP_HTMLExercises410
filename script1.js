90729b3949c0e7dbb0b2472c730fb8a6112e6330
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 20:07:39 +0300

    Поиск ошибок в коде со стандартными методами

--- script1.js
+++ script1.js
@@ -1,32 +1,62 @@
 "use strict";
 
-// let str = 'Hello world';
-// let res = str.slice(0) + str.slice(-1).toUpperCase();
 
-// console.log(res)
+console.log('====================== 1 ========================');
 
+let num = '12345';
+let arr = num.split('');
+let sum = 0;
 
-// let str = 'Hello world';
-// let res = str.slice(0, 2).toUpperCase() + str.slice(2);
+for (let digit of arr) {
+   sum += Number(digit);
+}
 
-// console.log(res2)
+console.log(sum);
 
-// let str = 'London';
-// let res = str.slice(0, 1).toLowerCase() + str.slice(1);
+console.log('====================== 2 ========================');
 
-// console.log(res)
+let num1 = 12345;
+let arr1 = String(num1).split('');
+let sum1 = 0;
 
+for (let digit of arr1) {
+   sum1 += Number(digit);
+}
 
-// let str = 'word1 word2 word3';
+console.log(sum1);
 
-// console.log(str)
+console.log('====================== 3 ========================');
 
-// let word = str.split(' ');
+let num2 = 12345;
+let arr2 = String(num2).split('');
+let sum2 = 0;
 
-// for (let i = 0; i <= word.length - 1; i++) {
-//    word[i] = word[i].slice(0, 1).toUpperCase() + word[i].slice(1);
-// };
+for (let digit of arr2) {
+   sum2 += Number(digit);
+}
 
-// str = word.join(' ');
+console.log(sum2);
 
-// console.log(str)
+console.log('====================== 4 ========================');
+
+let num3 = 12345;
+let arr3 = String(num3).split('');
+
+let sum3 = 0;
+for (let digit of arr3) {
+   sum3 += Number(digit);
+}
+
+console.log(sum3);
+
+console.log('====================== 5 ========================');
+
+let num4 = 12345;
+let arr4 = String(num4).split('');
+let prod = 1;
+
+for (let digit of arr4) {
+   prod *= Number(digit);
+}
+
+console.log(prod);
