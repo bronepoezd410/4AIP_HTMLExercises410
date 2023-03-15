ea55bbbcb63af35651a7a5b99df9de7355d6f9cc
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 19:11:02 +0300

    Заполнение многомерных массивов по порядку

--- script1.js
+++ script1.js
@@ -2,10 +2,25 @@
 
 // let arr = [];
 
-// for (let i = 0; i < 3; i++) {
+// for (let i = 0, k = 2; i < 3; i++) {
 //    arr[i] = [];
-//    for (let j = 1; j <= 5; j++) {
-//       arr[i].push(j);
+
+//    for (let j = 0; j < 3; j++) {
+//       arr[i].push(k++);
+//    }
+// }
+
+// console.log(arr);
+
+// let arr = [];
+// let k = 0;
+
+// for (let i = 0; i < 4; i++) {
+//    arr[i] = [];
+
+//    for (let j = 0; j < 3; j++) {
+//       k += 2
+//       arr[i].push(k);
 //    }
 // }
 
@@ -13,24 +28,14 @@
 
 // let arr = [];
 
-// for (let i = 0; i < 3; i++) {
-//    arr[i] = [];
-
-//    for (let j = 1; j <= 5; j++) {
-//       arr[i].push(j);
-//    }
-// }
-
-// console.log(arr);
-
-// let arr = [];
-
-// for (let i = 0; i < 3; i++) {
-//    arr[i] = [];
-
-//    for (let j = 1; j <= 5; j++) {
-//       arr[i].push(j);
-//    }
-// }
+// for (let i = 0, p = 1; i < 2; i++) {
+//    arr[i] = []
+//    for (let j = 0; j < 2; j++) {
+//       arr[i][j] = [];
+//       for (let k = 0; k < 2; k++) {
+//          arr[i][j].push(p++);
+//       };
+//    };
+// };
 
 // console.log(arr);
