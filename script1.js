ea01b49a21862015830cd568c2f7085b1d697a2f
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 20:09:06 +0300

    Параметры функций

--- script1.js
+++ script1.js
@@ -1,6 +1,18 @@
 "use strict";
 
 
-function me () {
-  console.log('Aloga Dance');
-}
+// function cube(a) {
+//    console.log(Math.pow(a, 3));
+// }
+
+// cube(5);
+
+// function negative(b) {
+//    if (b >= 0) {
+//       console.log('+++')
+//    } else {
+//       console.log('---')
+//    }
+// }
+
+// negative(-12);
