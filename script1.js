e81da130d2e3d296f4ee3f2bfe3d7bc74674a053
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 20:06:32 +0300

    Практика на использования изученных методов

--- script1.js
+++ script1.js
@@ -1,19 +1,32 @@
 "use strict";
 
-// let arr = [1, 2, 3];
-// arr.push(4, 5, 6);
+// let str = 'Hello world';
+// let res = str.slice(0) + str.slice(-1).toUpperCase();
 
-// console.log(arr);
-
-
-// let arr = [1, 2, 3];
-// arr.unshift(4, 5, 6);
-
-// console.log(arr);
+// console.log(res)
 
 
-// let arr = [1, 2, 3];
-// console.log(arr.shift());
+// let str = 'Hello world';
+// let res = str.slice(0, 2).toUpperCase() + str.slice(2);
 
-// let arr = [1, 2, 3];
-// console.log(arr.pop());
+// console.log(res2)
+
+// let str = 'London';
+// let res = str.slice(0, 1).toLowerCase() + str.slice(1);
+
+// console.log(res)
+
+
+// let str = 'word1 word2 word3';
+
+// console.log(str)
+
+// let word = str.split(' ');
+
+// for (let i = 0; i <= word.length - 1; i++) {
+//    word[i] = word[i].slice(0, 1).toUpperCase() + word[i].slice(1);
+// };
+
+// str = word.join(' ');
+
+// console.log(str)
