e4d51b6a8deba5438795c723dd512cdd68700196
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 20:10:00 +0300

    Параметры-переменные функций

--- script1.js
+++ script1.js
@@ -1,7 +1,12 @@
 "use strict";
 
-function three(a, b, c) {
-  return console.log(a + b + c);
-}
 
-three(2, 2, 2);
+let aa = 1;
+let bb = 5;
+let cc = 3;
+
+function three(a, b, c) {
+   return console.log(a + b + c);
+};
+
+three(aa, bb, cc);
