30e409157974db30e10737b5fafacfffc77deece
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 20:04:28 +0300

    Строковые методы

--- script1.js
+++ script1.js
@@ -1,52 +1,27 @@
-// "use strict";
-
-// console.log(Math.pow(2, 10));
-// console.log(Math.sqrt(245));
-// let arr = [4, 2, 5, 19, 13, 0, 10];
-// let result = 0;
-// for (let item of arr) {
-//    result += (Math.pow(item, 3));
-// }
-// console.log(Math.sqrt(result));
-// let num = 245;
-// let result = Math.round(Math.sqrt(num));
-// console.log(result
-//   );
-// let result = Math.sqrt(num);
-// console.log(result.toPrecision(3));
-
-// let result = Math.sqrt(num);
-// console.log(result.toPrecision(4));
-
-// let num1 = 587;
-// let obj = {
-//    floor: null,
-//    ceil: null,
-// }
-// let result = Math.sqrt(num)
-// obj.floor = Math.floor(result);
-// let result = Math.sqrt(num);
-// obj.ceil = Math.ceil(result);
-
-// console.log(obj)
-
-// console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
-// console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+"use strict";
 
 
-// function getRandomInt(min, max) {
-//    return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
 
-// console.log(getRandomInt(1, 100))
+// let str = 'фыв';
 
-// let newArr = [];
+// console.log(str.toUpperCase());
 
-// for (let i = 1; i <= 10; i++) {
-//    newArr.push(getRandomInt(1, 1000));
-// };
 
-// console.log(newArr)
-// let a = -12, b = -6;
+// let str = 'ФЫВ';
 
-// console.log(Math.abs(a - b));
+// console.log(str.toLowerCase());
+
+
+// let str = 'фдщфыдвщдафщыдващфывда!';
+
+// let a = str.substr(2, 3);
+// let a = str.substr(6, 10);
+// console.log(a, a);
+
+// let b = str.substring(2, 5);
+// let b = str.substring(6, 16);
+// console.log(b, b);
+
+// let c = str.slice(2, 5);
+// let c = str.slice(6, 16);
+// console.log(c, c);
