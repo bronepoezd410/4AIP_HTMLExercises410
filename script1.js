441ca055206369934936b08bae4073e43c12407e
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-15 18:48:49 +0300

    139_Перебор многомерных массивов

--- script1.js
+++ script1.js
@@ -1,44 +1,34 @@
 "use strict";
 
 // let arr = [
-// 	['a', 'b', 'c'],
-// 	['d', 'e', 'f'],
-// 	['g', 'h', 'i'],
-// 	['j', 'k', 'l'],
+//    [1, 2, 3],
+//    [4, 5],
+//    [6]
 // ];
 
-// console.log(arr[3][2] + arr[1][1] + arr[2][0] +arr[0][0]);
-
-// let later = [[1, 2], [3, 4], [5, 6]];
 // let sum = 0;
 
-// for (let i = 0; i < later.length; i++) {
-//   for (let j = 0; j < later[i].length; j++) {
-//     sum += later[i][j];
-//   }
-// }
+// for (let Arr of arr) {
+//    for (let nArr of Arr) {
+//       sum += nArr
+//    };
+// };
 
-// console.log(sum); // Output: 21
+// console.log(sum)
 
-let letarr = [
-   [
-     [1, 2],
-     [3, 4],
-   ],
-   [
-     [5, 6],
-     [7, 8],
-   ],
- ];
- 
- let sum = 0;
- 
- for (let i = 0; i < letarr.length; i++) {
-   for (let j = 0; j < letarr[i].length; j++) {
-     for (let k = 0; k < letarr[i][j].length; k++) {
-       sum += letarr[i][j][k];
-     }
-   }
- }
- 
- console.log(sum); // Output: 36
+
+// let arr = [
+//    [[1, 2], [3, 4]],
+//    [[5, 6], [7, 8]]
+// ];
+
+// let sum = 0;
+
+// for (let Arr of arr) {
+//    for (let nArr of Arr) {
+//       for (let inArr of nArr) {
+//          sum += inArr
+//       };
+//    };
+// };
+// console.log(sum)
