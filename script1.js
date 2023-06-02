221f81316822d2682516ed3a0af81f4bc8408890
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:06:30 +0300

    Объект с анонимными функциями в JavaScript

--- script1.js
+++ script1.js
@@ -1,24 +1,53 @@
 
 console.log('||| 1 |||==');
 
-let arr = [
-   function () { return 1 },
-   function () { return 2 },
-   function () { return 3 },
-];
+let newobj = {
+   func1: function () { return 1 },
+   func2: function () { return 2 },
+   func3: function () { return 3 },
+};
+
+let sum = newobj.func1() + newobj.func2() + newobj.func3();
+
+console.log(sum);
 
 console.log('||| 2 |||==');
 
-console.log(arr[2]());
+for (let elem in newobj) {
+   console.log(newobj[elem]())
+};
 
 console.log('||| 3 |||==');
 
-let sum = arr[0]() + arr[1]() + arr[2]();
+let arrNum = [1, 2, 3, 4, 5];
 
-console.log(sum)
+const calcArr = {
 
-console.log('||| 4 |||==');
+   sumArr: function (arr) {
+      let sum = 0;
+      for (let elem of arr) {
+         sum += elem
+      }
+      return sum;
+   },
 
-for (let func of arr) {
-   console.log(func());
+   sumArrSquare: function (arr) {
+      let sum = 0;
+      for (let elem of arr) {
+         sum += Math.pow(elem, 2)
+      }
+      return sum;
+   },
+
+   sumArrCube: function (arr) {
+      let sum = 0;
+      for (let elem of arr) {
+         sum += Math.pow(elem, 3)
+      }
+      return sum;
+   }
 };
+
+console.log(`Сумма элементов массива ${arrNum} = ${ calcArr.sumArr(arrNum) }`);
+console.log(`Сумма квадратов элементов массива ${arrNum} = ${ calcArr.sumArrSquare(arrNum) }`);
+console.log(`Сумма кубов элементов массива ${arrNum} = ${ calcArr.sumArrCube(arrNum) }`)
