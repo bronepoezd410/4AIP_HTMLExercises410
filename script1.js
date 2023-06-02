e5e9c5105f4c66e672b8a70835a3442c0e86b608
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:23:47 +0300

    Смена формата даты в JavaScript

--- script1.js
+++ script1.js
@@ -1,16 +1,20 @@
+          
+         let date = new Date();
 
-let date = new Date();
+         console.log("||| 1 |||==");
 
-console.log("||| 1 |||==");
+         function addZero(num) {
+            if (num >= 0 && num <= 9) {
+               return "0" + num;
+            } else {
+               return num;
+            }
+         }
 
-function addZero(num) {
-   if (num >= 0 && num <= 9) {
-      return "0" + num;
-   } else {
-      return num;
-   }
-}
+         let date1 = addZero(date.getDate()) +"-" + addZero(date.getMonth() + 1) +"-" + date.getFullYear();
 
-console.log(
- addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds()) + ' ' +
- addZero(date.getDate()) + '.' + addZero(date.getMonth() + 1) + '.' + date.getFullYear());
+         console.log(date1);
+
+         let date2 = date1.split("-").join(".");
+
+         console.log(date2);
