a10ca7eb3cbd3744362be372e8f22104917b800b
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:27:56 +0300

    Сравнение объектов с датами в JavaScript

--- script1.js
+++ script1.js
@@ -1,37 +1,5 @@
 
-console.log("||| 1 |||==");
+let date1 = new Date(2020, 1, 1);
+let date2 = new Date(2019, 1, 1);
 
-let date1 = "20201131";
-let date2 = "20201201";
-
-if (date1 > date2) {
-   console.log("2020.11.31 больше");
-} else {
-   console.log("2020.12.01 больше");
-}
-
-console.log("||| 2 |||==");
-
-let date3 = "0921";
-let date4 = "0923";
-
-if (date3 > date4) {
-   console.log("09-21 больше");
-} else {
-   console.log("09-23 больше");
-}
-
-console.log("||| 3 |||==");
-let date = "03-20";
-
-if (date >= "01-01" && date <= "03-08") {
-   console.log("Водолей");
-}
-
-if (date >= "03-09" && date <= "06-17") {
-   console.log("Рыбы");
-}
-
-if (date >= "06-18" && date <= "12-31") {
-   console.log("Козерог");
-}
+console.log(date1 > date2); // выведет true
