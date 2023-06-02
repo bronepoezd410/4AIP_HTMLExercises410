e0e93c0114b4c55a1c22b71b98d82c941ba51acc
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:13:50 +0300

    Метод filter для фильтрации массива в JavaScript

--- script1.js
+++ script1.js
@@ -1,11 +1,64 @@
 
 console.log("||| 1 |||==");
 
-let arr = [1, 2, 3, 4, 5];
-let sum = 0;
+const arr1 = [1, 2, -3, 4, -5];
 
-arr.forEach(function (elem) {
-   sum += Math.pow(elem, 2);
+let result1 = arr1.filter((elem) => elem >= 0);
+
+console.log(result1);
+
+console.log("||| 2 |||==");
+
+let result2 = arr1.filter((elem) => elem < 0);
+
+console.log(result2);
+
+console.log("||| 3 |||==");
+
+const arr2 = [1, 2, -3, -4, 11, 20];
+
+let result3 = arr2.filter((elem) => elem > 0 && elem < 10);
+
+console.log(result3);
+
+console.log("||| 4 |||==");
+
+const arr3 = ["hello", "world", "a", "b"];
+
+let result4 = arr3.filter((elem) => elem.length >= 5);
+
+console.log(result4);
+
+console.log("||| 5 |||==");
+
+const arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+let result5 = arr5.filter((elem, index) => elem * index <= 30);
+
+console.log(result5);
+
+console.log("||| 6 |||==");
+
+const arr6 = [1, 2, [3, 4], 5, [6, 7]];
+const newarr6 = [];
+
+let result6 = arr6.filter(function (elem) {
+   if (typeof elem !== "object") {
+      newarr6.push(elem);
+   }
 });
 
-console.log(sum);
+console.log(newarr6);
+
+console.log("||| 7 |||==");
+
+const arr7 = [1, 2, -3, -4, -11, 20];
+let count = 0;
+
+let result7 = arr7.filter((elem) => {
+   if (elem < 0) {
+      count++;
+   }
+});
+
+console.log(count);
