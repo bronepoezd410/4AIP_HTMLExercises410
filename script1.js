70561bd15a03aa4602b94b614e58369bf8335104
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:13:09 +0300

    Метод forEach для перебора массива в JavaScript

--- script1.js
+++ script1.js
@@ -1,52 +1,11 @@
+
 console.log("||| 1 |||==");
 
-let arr1 = [1, 2, 3, 4, 5];
-let newArr1 = [];
+let arr = [1, 2, 3, 4, 5];
+let sum = 0;
 
-let result = arr1.map(function (elem) {
-   newArr1.push(Math.pow(elem, 2));
+arr.forEach(function (elem) {
+   sum += Math.pow(elem, 2);
 });
 
-console.log(newArr1);
-
-console.log("||| 2 |||==");
-
-let arr2 = ["a", "b", "c", "d"];
-
-let result2 = arr2.map(function (elem) {
-   return elem + "!";
-});
-
-console.log(result2);
-
-console.log("||| 3 |||==");
-
-let arr3 = ["hello", "world", "andrey"];
-
-let result3 = arr3.map(function (elem) {
-   return elem.split("").reverse().join("");
-});
-
-console.log(result3);
-
-console.log("||| 4 |||==");
-
-let arr4 = ["123", "456", "789"];
-let newArr4 = [];
-
-let result5 = arr4.map(function (elem) {
-   elem.split("");
-   return newArr4.push(Number(elem));
-});
-
-console.log(newArr4);
-
-console.log("||| 5 |||==");
-
-let arr6 = [1, 2, 3, 4, 5];
-
-let result6 = arr6.map(function (elem, index) {
-   return elem * index;
-});
-
-console.log(result6);
+console.log(sum);
