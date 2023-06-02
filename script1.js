2cb788630511b979c111cb27a9f45a3e8ae9cb06
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 21:52:29 +0300

    Цикл и return

--- script1.js
+++ script1.js
@@ -1,30 +1,10 @@
-function func1(num) {
-	return num;
-	
-	let res = num ** 2;
-	return res;
-}
-
-console.log( func1(3) );
-// 2
-function func2(num) {
-	if (num <= 0) {
-		return Math.abs(num);
-	} else {
-		return num ** 2;
-	}
-}
-
-console.log( func2(10) );
-console.log( func2(-5) );
-// 3
 function func(num) {
-	if (num <= 0) {
-		return Math.abs(num);
-	}
+	let sum = 0;
 	
-	return num ** 2;
+	for (let i = 1; i <= num; i++) {
+		sum += i;
+	}
+    return sum;
 }
 
-console.log( func(10) );
-console.log( func(-5) );
+console.log( func(5) );
