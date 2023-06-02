fca2fa657916df170aee1954e318557e6af6afac
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:23:18 +0300

    Работа с объектом Date в JavaScript

--- script1.js
+++ script1.js
@@ -1,61 +1,14 @@
+
+let date = new Date();
+
 console.log("||| 1 |||==");
 
-function func1([name, surname, department, position, salary]) {
-   console.log(name);
-   console.log(department);
-}
-
-func1(["John", "Smit", "development", "programmer", 2000]);
+console.log(`Текущий день: ${date.getDate()}`)
 
 console.log("||| 2 |||==");
 
-function func2([name, surname, ...info]) {
-   console.log(name);
-   console.log(surname);
-   console.log(info);
-}
-
-func2(["John", "Smit", "development", "programmer", 2000]);
+console.log(`Текущий месяц: ${date.getMonth() + 1}`)
 
 console.log("||| 3 |||==");
 
-function func3([name, surname, department, position = "мид"]) {
-   console.log(name);
-   console.log(position);
-   console.log(department);
-}
-
-func3(["встань", "Smit", "да"]);
-
-console.log("||| 4 |||==");
-
-function func(department, user, surname, year, month, day) {
-   console.log(department);
-   console.log(user);
-   console.log(surname);
-   console.log(year);
-   console.log(month);
-   console.log(day);
-}
-
-func("development", ...["John", "Smit"], ...[2018, 12, 31]);
-
-console.log("||| 5 |||==");
-
-function func5({ color: c, width: w, height: h }) {
-   console.log(c);
-   console.log(w);
-   console.log(h);
-}
-
-func5({ color: "red", width: 400, height: 500 });
-
-console.log("||| 6 |||==");
-
-function func6({ color: c = 'black', width: w, height: h }) {
-   console.log(c);
-   console.log(w);
-   console.log(h);
-}
-
-func6({width: 400, height: 500 });
+console.log(`Текущий год: ${date.getFullYear()}`)
