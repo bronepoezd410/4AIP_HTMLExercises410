bafe0ec3de7abe11a69181efdb81e713c074d970
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:06:10 +0300

    Массив с анонимными функциями в JavaScript

--- script1.js
+++ script1.js
@@ -1,196 +1,24 @@
 
 console.log('||| 1 |||==');
 
-function func1() {
-   console.log('!');
-};
-
-// Function Declaration
+let arr = [
+   function () { return 1 },
+   function () { return 2 },
+   function () { return 3 },
+];
 
 console.log('||| 2 |||==');
 
-let func2 = function () {
-   console.log('!');
-};
-
-// Function Expression
+console.log(arr[2]());
 
 console.log('||| 3 |||==');
 
-+function () {
-   console.log('!');
-};
+let sum = arr[0]() + arr[1]() + arr[2]();
 
-// Function Expression
+console.log(sum)
 
 console.log('||| 4 |||==');
 
-!function func4() {
-   console.log('!');
+for (let func of arr) {
+   console.log(func());
 };
-
-// Function Expression
-
-console.log('||| 5 |||==');
-
--function func5() {
-   console.log('!');
-};
-
-// Function Expression
-
-console.log('||| 6 |||==');
-
-1 + function func6() {
-   console.log('!');
-};
-
-// Function Expression
-
-console.log('||| 7 |||==');
-
-(function func7() {
-   console.log('!');
-});
-
-// Function Expression
-
-console.log('||| 8 |||==');
-
-console.log(
-   function () {
-      console.log('!');
-   }
-);
-
-// Function Expression
-
-console.log('||| 9 |||==');
-
-let test = function func() {
-   console.log('!');
-};
-
-// Function Expression
-
-console.log('||| 10 |||==');
-
-console.log(
-   function func() {
-      console.log('!');
-   }
-);
-
-// Function Expression
-
-console.log('||| 11 |||==');
-
-+function func11() {
-   console.log('!');
-};
-
-// Function Expression
-
-console.log('||| 12 |||==');
-
-function func12() {
-   console.log('!');
-};
-
-//Function Declaration
-
-console.log('||| 13 |||==');
-
--
-   function func13() {
-      console.log('!');
-   }
-
-// Function Expression 
-
-console.log('||| 14 |||==');
-
--1;
-function func14() {
-   console.log('!');
-};
-
-//Function Declaration
-
-console.log('||| 15 |||==');
-
--1
-function func15() {
-   console.log('!');
-}
-
-//Function Declaration
-
-console.log('||| 16 |||==');
-
-1
-function func16() {
-   console.log('!');
-}
-
-//Function Declaration
-
-console.log('||| 17 |||==');
-
--1 -
-   function func17() {
-      console.log('!');
-   }
-
-// Function Expression 
-
-console.log('||| 18 |||==');
-
-function func18() {
-   console.log('!');
-}
-+1;
-
-//Function Declaration
-
-console.log('||| 19 |||==');
-
-function func19() {
-   console.log('!');
-} + 1;
-
-// Function Declaration
-
-console.log('||| 20 |||==');
-
-+function func20() {
-   console.log('!');
-} + 1;
-
-// Function Expression 
-
-console.log('||| 21 |||==');
-
-+
-   function func21() {
-      console.log('!');
-   } + 1;
-
-// Function Expression 
-
-console.log('||| 22 |||==');
-
-+ 1
-function func22() {
-   console.log('!');
-} + 1;
-
-// Function Declaration
-
-console.log('||| 23 |||==');
-
-function func23() {
-   console.log('!');
-} + console.log('!');
-
-// Function Declaration
