27828e272abe7a93468bcb08553cfc6ef44769ef
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:15:47 +0300

    Оператор spread в JavaScript

--- script1.js
+++ script1.js
@@ -1,24 +1,48 @@
+
 console.log("||| 1 |||==");
 
-function func1(num1, num2, num3, num4, num5) {
-   return num1 + num2 + num3 + num4 + num5;
-}
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
 
-let arr = [1, 2, 3, 4, 5];
+let arr = ["a", ...arr1, "b", "c", ...arr2];
+console.log(arr);
 
-console.log(func1(...arr));
+// ['a', 1, 2, 3, 'b', 'c', 4, 5, 6]
 
 console.log("||| 2 |||==");
 
-function func2(n1, n2, n3, n4, n5, n6, n7, n8) {
-   return (n1 + n2 + n3 + n4) * (n5 + n6 + n7 + n8);
-}
+let arr21 = [1, 2, 3];
+let arr22 = [4, 5, 6];
 
-console.log(func2(1, ...[2, 3, 4], 5, ...[6], ...[7, 8])); // 260
+let arr23 = ["a", ...arr21, ...arr22, "b", "c"];
+console.log(arr23);
+
+// ["a", 1, 2, 3, 4, 5, 6, "b", "c"]
 
 console.log("||| 3 |||==");
 
-let arr3 = [1, 2, 3, 4, 5];
-let min = Math.min(...arr3);
+let arr31 = [1, 2, 3];
+let arr32 = [...arr31, 4, 5, 6];
 
-console.log(min)
+let arr33 = ["a", "b", "c", ...arr32];
+console.log(arr33);
+
+// ["a", "b", "c", 1, 2, 3, 4, 5, 6]
+
+console.log("||| 4 |||==");
+
+let arr41 = [1, 2, 3];
+let arr42 = [...arr41, 4, 5, 6];
+let arr43 = [...arr42, 7, 8, 9];
+
+let arr44 = [0, ...arr43];
+console.log(arr44);
+
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+console.log("||| 5 |||==");
+
+let arr51 = [1, 2, 3];
+let arr52 = [...arr1];
+
+console.log(arr52);
