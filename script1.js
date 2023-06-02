b045110c98a97d5854862515759a2e1b37adc62e
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:26:04 +0300

    Автоматическая корректировка дат в JavaScript

--- script1.js
+++ script1.js
@@ -1,20 +1,59 @@
-let now = new Date();
-
 console.log("||| 1 |||==");
 
-let date = new Date(2000, 8, 31, 23, 59, 59);
-let date2 = new Date(2010, 2, 14, 23, 59, 59);
-let diff = date2 - date;
-console.log(diff);
+let date1 = new Date(2018, 1, 35);
+console.log(date1); // 07.03.2018
 
 console.log("||| 2 |||==");
 
-console.log(diff / (1000 * 3600 * 24));
+let date2 = new Date(2018, 15, 1);
+console.log(date2); // 01.04.2019
 
 console.log("||| 3 |||==");
 
-console.log(diff / (1000 * 60 * 60 * 24 * 31));
+let date3 = new Date(2018, 3, 31);
+console.log(date3); // 01.05.2018
 
 console.log("||| 4 |||==");
 
-console.log(diff / (1000 * 60 * 60 * 24 * 31 * 12));
+let date4 = new Date(2018, 1, 31);
+console.log(date4); // 03.03.2018
+
+console.log("||| 5 |||==");
+
+let date5 = new Date(2018, 12, 33);
+console.log(date5); // 02.02.2019
+
+console.log("||| 6 |||==");
+
+let date6 = new Date(2018, 33, 33);
+console.log(date6); // 02.11.2020
+
+console.log("||| 7 |||==");
+
+let date7 = new Date(2018, 5, 0);
+console.log(date7); // 31.05.2018
+
+console.log("||| 8 |||==");
+
+let date8 = new Date(2018, 0, 0);
+console.log(date8); // 31.12.2017
+
+console.log("||| 9 |||==");
+
+let date9 = new Date(2018, -12, -33);
+console.log(date9); // 29.11.2016
+
+console.log("||| 10 |||==");
+
+let date10 = new Date(2018, 0, 1, 23, 0, 60);
+console.log(date10); // 01.01.2018 23:01:00
+
+console.log("||| 11 |||==");
+
+let date11 = new Date(2018, 0, 1, 23, 60, 0);
+console.log(date11); // 02.01.2018 00:00:00
+
+console.log("||| 12 |||==");
+
+let date12 = new Date(2018, 0, 1, 100, 100, 100);
+console.log(date12); // 05.01.2018 05:41:40 
