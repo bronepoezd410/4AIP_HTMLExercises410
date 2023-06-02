49b85c1c458cb3ff6b67cbf0ad16f8699b71986c
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:24:34 +0300

    Вывод частей даты словом в JavaScript

--- script1.js
+++ script1.js
@@ -1,18 +1,8 @@
           
-         let date = new Date();
-
          console.log("||| 1 |||==");
 
-         console.log(`Номер текущего дня недели ${date.getDay()}`);
+         let date = new Date();
 
-         console.log("||| 2 |||==");
+         let months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
 
-         if (date.getDay() == 0 || date.getDay() == 6) {
-          console.log(`${date.getDay()} - Выходной день`)
-         } else {
-          console.log(`${date.getDay()} - Рабочий день`)
-         }
-
-         console.log("||| 3 |||==");
-
-         console.log(`До воскресенья осталось ${6 - date.getDay()}`)
+         console.log(`Текущий месяц - ${months[date.getMonth()]}`)
