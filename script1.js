57fcc3c25e188bbb38df75a2ff147643bbeb8f28
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 21:53:56 +0300

    Флаги в функциях

--- script1.js
+++ script1.js
@@ -1,9 +1,47 @@
-function func(num1, num2) {
-	if (num1 > 0 && num2 > 0) {
-		return num1 * num2;
-	} else {
-		return num1 - num2;
-	}
+let arr1 = [2, 4, 6];
+function func1(arr) {
+for (let elem of arr) {
+    if (elem % 2 != 0) {
+        return false;
+    }
+};
+    return true;
+};
+if (func1(arr1)) {
+    console.log('Все числа в массиве четные');
+} else {
+    console.log('В массиве есть нечетные числа')
 }
-
-console.log(func(3, 4));
+// 2
+let num = 4663;
+function func2(number) {
+let arrNum = String(number).split('')
+    for (let elem of arrNum) {
+        if (elem % 2 != 0) {
+            return false;
+        }
+    };
+    return true;
+    };
+    if (func2(num)) {
+        console.log('Все цифры в числе четные');
+    } else {
+        console.log('В числе есть нечетные цифры')
+    }
+// 3
+let arr3 = [2, 3, 4, 4, 7];
+function func3(arr) {
+    for (let i = 0; i <= arr.length - 1; i++) {
+        if (arr[i] == arr[i+1]) {
+            return true;
+        } else {
+            continue;
+        }
+    };
+        return false;
+    }
+if (func3(arr3)) {
+    console.log('В этом масиве есть два одинаковых элемента подряд');
+} else {
+    console.log('В этом масиве нет двух одинаковых элемента подряд')
+}
