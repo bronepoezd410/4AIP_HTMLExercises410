8ec9a384d3ba37a7aac121e99e0067cb94c2e354
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:14:47 +0300

    Метод every для проверки массива в JavaScript

--- script1.js
+++ script1.js
@@ -1,14 +1,13 @@
-
 console.log("||| 1 |||==");
 
-let arr1 = [2, 4, 6, 8];
+let arr = [0, 0, 0];
 
-let result1 = arr1.every((elem) => elem > 0);
-console.log(result1);
+let result = arr.some((elem) => elem > 0);
+console.log(result);
 
 console.log("||| 2 |||==");
 
 let arr2 = [2, 4, 6, 8, 30];
 
-let result2 = arr2.every((elem, index) => elem * index < 30);
+let result2 = arr2.some((elem, index) => elem * index < 30);
 console.log(result2);
