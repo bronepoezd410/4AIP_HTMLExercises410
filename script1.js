d9a6aad6062ad2ad269b0d503da6bdc70a640622
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:16:44 +0300

    Оператор rest в JavaScript

--- script1.js
+++ script1.js
@@ -1,30 +1,8 @@
 
-console.log("||| 1 |||==");
+func(1, 2, 3, 4, 5);
 
-let arr = [..."12345"];
-console.log(arr);
-
-// ['1', '2', '3', '4', '5']
-
-console.log("||| 2 |||==");
-
-let arr2 = ["a", ..."12345"];
-console.log(arr2);
-
-// ["a", '1', '2', '3', '4', '5']
-
-console.log("||| 3 |||==");
-
-let arr3 = [..."12345", ..."56789"];
-console.log(arr3);
-
-// ['1', '2', '3', '4', '5', '5', '6', '7', '8', '9']
-
-console.log("||| 4 |||==");
-
-let arr41 = ["a", "b", "c"];
-let arr42 = [...arr41, ..."12345"];
-
-console.log(arr42);
-
-// ["a", "b", "c", '1', '2', '3', '4', '5']
+function func(a, b, ...rest) {
+   console.log(a);             // выведет 1
+   console.log(b);             // выведет 2
+   console.log(rest);          // выведет [3, 4, 5]
+}
