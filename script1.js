b2cfd26d543f8827fe2733a9583ec967d796ce7f
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 21:55:31 +0300

    Поиск ошибок в коде с функциями

--- script1.js
+++ script1.js
@@ -1,36 +1,36 @@
-let arr1 = [1, 2, 3, 4];
-function calcArr(arr) {
-let res = 0;
-for (let elem of arr) {
-    res += elem;
-};
-res = res / arr.length
-    return res;
-};
-console.log(calcArr(arr1))
+function func11() {
+  return 3;
+}
+function func12() {
+  return 5;
+  }
+console.log(func11() + func12());
 // 2
-let arr2 = [1, 2, 3, 4];
-let arr3 = [5, 6, 7, 8];
-function sumElem(arr) {
-    let sum = 0;
-    for (let elem of arr) {
-        sum += elem
-    };
-    return sum;
-};
-function calcArr__1(arr1, arr2) {
-    let res1 = sumElem(arr1)
-    let res2 = sumElem(arr2)
-    return res1 / res2;
-};
-console.log(calcArr__1(arr2, arr3));
+function sum2(arr) {
+  let res = 0;
+for (let elem of arr) {
+  res += elem;
+}
+  return res;
+}
+console.log(sum2([1, 2, 3, 4, 5]));
 // 3
-let arr6 = [1, 2, 3, 4, 5, 6]
-function calcArr_2(arr) {
-    let res = 1;
-    for (let elem of arr) {
-        res *= elem;
-    }
-    return res;
+let arr = [1, 2, 3, 4, 5];
+function func3(array) {
+  let res = 0;
+for (let elem of array) {
+  res += elem;
 };
-console.log(calcArr_2(arr6));
+return res;
+};
+console.log(func3(arr))
+// 4
+let arr5 = [1, 2, 3, 4, 5];
+console.log(sum5(arr5));
+function sum5(arr) {
+  let sum = 0;
+for (let elem of arr) {
+  sum += elem;
+}
+return sum;
+}
