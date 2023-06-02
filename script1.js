6bb3d6c8d59d46361949ffa1a4da9da0f7b6b085
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 22:25:42 +0300

    Разница между объектами с датой в JavaScript

--- script1.js
+++ script1.js
@@ -2,15 +2,19 @@ let now = new Date();
 
 console.log("||| 1 |||==");
 
-let date = new Date(1988, 1, 4, 23, 59, 59);
-let date2 = new Date(2000, 1, 10, 23, 59, 59);
-
-let diff = date2.getTime() - date.getTime();
-console.log(diff / (1000 * 3600 * 24));
+let date = new Date(2000, 8, 31, 23, 59, 59);
+let date2 = new Date(2010, 2, 14, 23, 59, 59);
+let diff = date2 - date;
+console.log(diff);
 
 console.log("||| 2 |||==");
 
-let now1 = new Date();
-let date1 = new Date(2003, 8, 15, 23, 59, 59);
-let diff1 = now.getTime() - date1.getTime();
-console.log(diff1 / (1000 * 60 * 60 * 24 * 31));
+console.log(diff / (1000 * 3600 * 24));
+
+console.log("||| 3 |||==");
+
+console.log(diff / (1000 * 60 * 60 * 24 * 31));
+
+console.log("||| 4 |||==");
+
+console.log(diff / (1000 * 60 * 60 * 24 * 31 * 12));
