690470abf706b30d5c206954a0c6a016247ce563
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 21:50:42 +0300

    Последовательный вызов функций

--- script1.js
+++ script1.js
@@ -1,10 +1,24 @@
-"use strict";
+function sqrt(num) {
+	return Math.sqrt(num);
+}
 
-
-function calcCube(x) {
-  return Math.pow(x, 3)
-};
-
-let res = calcCube(3);
-
+function round(num) {
+	return num.toFixed(3);
+}
+let res = round(sqrt(2));
 console.log(res)
+// 2
+function sqrt1(num4) {
+	return Math.sqrt1(num4);
+}
+function sum(num1, num2, num3) {
+	return num1 + num2 + num3;
+}
+let res1 = sum(sqrt1(2), sqrt1(3), sqrt1(4));
+console.log(res1)
+// 3
+function round2(num) {
+    return num.toFixed(3);
+}
+let res2 = round2(sum1(sqrt1(2), sqrt1(3), sqrt1(4)));
+console.log(res2);
