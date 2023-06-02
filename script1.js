5b22173fb20957ee9566d132c327c6fa6aca897a
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-02 21:53:19 +0300

    Прием работы с return

--- script1.js
+++ script1.js
@@ -1,10 +1,9 @@
-let i = 0;
-function calc(x) {
-    for (;; i++) {
-        x = x / 2;
-        if (x <= 10) {
-            return i;
-        } 
-    }
+function func(num1, num2) {
+	if (num1 > 0 && num2 > 0) {
+		return num1 * num2;
+	} else {
+		return num1 - num2;
+	}
 }
-console.log(calc(100));
+
+console.log(func(3, 4));
